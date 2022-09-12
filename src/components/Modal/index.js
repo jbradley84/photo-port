7b0153df016294bdfac932c8d55aecc0f4234b42
jbradley84 <utbradley84@yahoo.com>
2022-09-12 12:37:23 -0500

    added toggle function to Modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+const Modal = ({ onClose, currentPhoto }) => {
    const {name, category, description, index} = currentPhoto;
 
    return (
@@ -9,12 +9,12 @@ function Modal({currentPhoto}) {
             <h3 className="modalTitle">{name}</h3>
             <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
             <p>{description}</p>
-            <button type="button">
+            <button type="button" onClick={onClose}>
                Close this modal
             </button>
          </div>
       </div >
    );
-}
+};
 
 export default Modal;
